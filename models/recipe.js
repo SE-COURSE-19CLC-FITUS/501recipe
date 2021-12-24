07268aa270181278a05338121e1d33e46cd14e5c
duckymomo20012 <tienvinh.duong4@gmail.com>
2021-12-24 20:42:24 +0700

    Add .env to gitignore

---
+++ models/recipe.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const recipeSchema = Schema({
+  imageUrl: String,
+  ingredients: [{ text: String }],
+  instructions: [{ text: String }],
+  publisher: String,
+  servings: { type: Number, min: [1, "Must at least 1, got {VALUE}"] },
+  source: String,
+  tags: [{ text: String }],
+  timePrep: String,
+  titleRecipe: String,
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema, {
+  collection: "recipe",
+});
